d25cac527029977c1b54240532fdd22d4018a41e
DispoOhnePlan <62263314+DispoOhnePlan@users.noreply.github.com>
2020-03-30 12:59:33 +0200

    Update index.user.js

--- DarkMap/index.user.js
+++ DarkMap/index.user.js
@@ -10,16 +10,21 @@
 (function() {
   'use strict';
   // ================
-  const dependingTime = true; // (true|false) zeitabhängig oder permanent
+  const dependingTime = false; // (true|false) zeitabhängig oder permanent
   const start = 22; // Beginn der DarMap in Stunden
   const end = 6; // Ende der DarMap in Stunden
+  const style = 0; // (0|1) Styleauswahl
   // ================
+  const stylefilter = [
+      "filter: invert(1) grayscale(1);-webkit-filter: invert(1) grayscale(1);",
+      "-webkit-filter: invert(1) hue-rotate(185deg) grayscale(0.5) brightness(0.9); filter: invert(1) hue-rotate(185deg) grayscale(0.5) brightness(0.9);"
+  ];
   function check() {
     var time = new Date();
     var h = time.getHours();
     if (h >= start && h < end) {
         if($("#darkmap").length == 0){
-            $("head").append("<style id='darkmap'>.leaflet-tile {-webkit-filter: invert(1) grayscale(.7);filter: invert(1) grayscale(.7);}</style>");
+            $("head").append("<style id='darkmap'>.leaflet-tile {"+stylefilter[style]+"}</style>");
         }
     }else{
         if($("#darkmap").length){
@@ -35,6 +40,6 @@
       check();
     }, 60 * 1000 * 5);
   } else {
-    $("head").append("<style>.leaflet-tile {-webkit-filter: invert(1) grayscale(.7);filter: invert(1) grayscale(.7);}</style>");
+    $("head").append("<style>.leaflet-tile {"+stylefilter[style]+"}</style>");
   }
 })();
